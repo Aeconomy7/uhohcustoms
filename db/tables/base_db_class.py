7a3fdad9c5456a3957bd137f51a316cb2481f1f1
aeconomy7 <aeconomy7@gmail.com>
2025-03-04 21:46:23 -0600

    cleaned up repo

--- db/tables/base_db_class.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from config import SCHEMA_NAME
-
-class BaseDbClass(DeclarativeBase):
-    __table_args__ = {"schema": SCHEMA_NAME} if SCHEMA_NAME else {}
-    
-    pass
